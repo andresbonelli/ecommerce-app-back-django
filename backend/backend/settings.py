751d84ca68f0003d1789f0adaccac9f61e1105a7
andresbonelli <donpipon@hotmail.com>
2024-08-08 13:29:31 -0300

    First Swagger implementation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_yasg",
     "api",
     "rest_framework",
     "corsheaders",
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Toco Backend API",
+      default_version='v1',
+      description="E-commerce Backend",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="donpipon@hotmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=[]
+)
 
 
 urlpatterns = [
@@ -13,7 +30,10 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name="get_token"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="refresh_token"),
     path('api-auth/', include("rest_framework.urls")),
-    path('administrator/', include("api.adminUrls"))
+    path('administrator/', include("api.adminUrls")),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
